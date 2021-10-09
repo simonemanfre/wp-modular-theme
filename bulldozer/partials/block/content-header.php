300eb25cf0a0d7aa12248438b0e930e1593471d3
simonemanfre <simone.manfredini@trapella.it>
2021-10-10 01:25:17 +0200

    sistemo blocco header

--- bulldozer/partials/block/content-header.php
+++ bulldozer/partials/block/content-header.php
@@ -4,7 +4,7 @@
  */ 
 ?>
 
-<header class="c-block-header">
+<header class="c-block-header c-block-header--<?php the_field('style') ?> <?php the_field('align') ?> u-wide-full">
     <?php if(get_field('title')): ?><h2 class="c-title t-title2"><?php the_field('title'); ?></h2><?php endif; ?>
     <?php if(get_field('subtitle')): ?><h3 class="c-subtitle"><?php the_field('subtitle'); ?></h3><?php endif; ?>
 </header>
