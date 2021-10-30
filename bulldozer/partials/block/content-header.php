8e3bbc161ff07715f66c7cce1f4643b71a5782cf
simonemanfre <simone.manfredini@trapella.it>
2021-10-30 15:52:15 +0200

    sistemo CSS

--- bulldozer/partials/block/content-header.php
+++ bulldozer/partials/block/content-header.php
@@ -4,7 +4,7 @@
  */ 
 ?>
 
-<header class="c-block-header c-block-header--<?php the_field('style') ?> <?php the_field('width') ?> <?php the_field('align') ?>">
+<section class="c-block-header c-block-header--<?php the_field('style') ?> <?php the_field('width') ?> <?php the_field('align') ?>">
     <?php if(get_field('title')): ?><h2 class="c-title t-title2"><?php the_field('title'); ?></h2><?php endif; ?>
     <?php if(get_field('subtitle')): ?><h3 class="c-subtitle"><?php the_field('subtitle'); ?></h3><?php endif; ?>
-</header>
+</section>
