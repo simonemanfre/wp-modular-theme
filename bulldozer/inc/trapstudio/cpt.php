592b4108a0553b204a4d081aeaa52ecd3bb40b6c
simonemanfre <simone.manfredini@trapella.it>
2021-10-30 16:21:39 +0200

    riordino file template

--- bulldozer/inc/trapstudio/cpt.php
+++ bulldozer/inc/trapstudio/cpt.php
@@ -21,7 +21,7 @@ function cptui_register_my_cpts() {
 		"show_in_rest" => true,
 		"rest_base" => "",
 		"rest_controller_class" => "WP_REST_Posts_Controller",
-		"has_archive" => true,
+		"has_archive" => false,
 		"show_in_menu" => true,
 		"show_in_nav_menus" => true,
 		"delete_with_user" => false,
--- bulldozer/inc/trapstudio/scripts.php
+++ bulldozer/inc/trapstudio/scripts.php
@@ -5,19 +5,19 @@
         $var_array = array();
 
         //FILE CSS
-        wp_enqueue_style('normalize', get_template_directory_uri() . "/normalize.css", array(), $dati_tema->Version);
+        wp_enqueue_style('normalize', get_template_directory_uri() . "/assets/css/normalize.css", array(), $dati_tema->Version);
         wp_enqueue_style('icons', get_template_directory_uri() . "/icons.css", array('normalize'), $dati_tema->Version);
-        wp_enqueue_style('slick', get_template_directory_uri() . "/slick.css", array('normalize'), $dati_tema->Version);
+        wp_enqueue_style('slick', get_template_directory_uri() . "/assets/css/slick.css", array('normalize'), $dati_tema->Version);
         wp_enqueue_style('style', get_stylesheet_uri(), array('normalize'), $dati_tema->Version);
-        wp_enqueue_style('responsive', get_template_directory_uri() . "/responsive.css", array('style'), $dati_tema->Version, '(min-width: 480px)');
+        wp_enqueue_style('responsive', get_template_directory_uri() . "/assets/css/responsive.css", array('style'), $dati_tema->Version, '(min-width: 480px)');
         
 		//FILE SCRIPTS
-        wp_enqueue_script( 'ofi', get_template_directory_uri() . "/js/ofi.min.js", array('jquery'), '1.0', true);
-        wp_register_script('siteScripts', get_template_directory_uri() . "/js/scripts.js", array('jquery'), '1.0', true );
+        wp_enqueue_script( 'ofi', get_template_directory_uri() . "/assets/js/ofi.min.js", array('jquery'), '1.0', true);
+        wp_register_script('siteScripts', get_template_directory_uri() . "/assets/js/scripts.js", array('jquery'), '1.0', true );
 
         //SLICK
         $var_array['slick'] = true;
-        wp_enqueue_script('slick', get_template_directory_uri() . "/js/slick.min.js", array('jquery'), '1.0', true);
+        wp_enqueue_script('slick', get_template_directory_uri() . "/assets/js/slick.min.js", array('jquery'), '1.0', true);
         
         //VARIABILI DA PASSARE A JS
         $var_array['home'] = get_bloginfo('url');
