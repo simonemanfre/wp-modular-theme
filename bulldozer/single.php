9363f2d8ae1254251768061752f82c805b61b877
simonemanfre <simone.manfredini@trapella.it>
2021-11-06 20:39:15 +0100

    aggiungo gestione sidebar

--- bulldozer/single.php
+++ bulldozer/single.php
@@ -4,9 +4,28 @@
 
         <?php get_template_part('partials/page', 'header'); ?>
 
-        <div class="c-post-content l-container">
-            <?php the_content() ?>
-        </div>
+        <?php 
+        //SIDEBAR
+        $sidebar = get_field('page_sidebar');
+        if($sidebar && is_active_sidebar($sidebar)): 
+        ?>
+
+            <div class="c-post-content l-container <?php the_field('align_sidebar') ?>">
+                <div class="u-flex-4">
+                    <?php the_content() ?>
+                </div>
+                <ul class="c-sidebar u-flex-1 <?php the_field('margin_sidebar') ?>">
+                    <?php dynamic_sidebar($sidebar) ?>
+                </ul>
+            </div>
+
+        <?php else: ?>
+
+            <div class="c-post-content l-container">
+                <?php the_content() ?>
+            </div>
+        
+        <?php endif; ?>
         
     <?php endwhile; endif; ?>
 
--- bulldozer/templates/p-container-small.php
+++ bulldozer/templates/p-container-small.php
@@ -6,9 +6,28 @@ get_header();
 
         <?php get_template_part('partials/page', 'header'); ?>
 
-        <div class="c-post-content l-container l-container--small">
-            <?php the_content() ?>
-        </div>
+        <?php 
+        //SIDEBAR
+        $sidebar = get_field('page_sidebar');
+        if($sidebar && is_active_sidebar($sidebar)): 
+        ?>
+
+            <div class="c-post-content l-container l-container--small <?php the_field('align_sidebar') ?>">
+                <div class="u-flex-4">
+                    <?php the_content() ?>
+                </div>
+                <ul class="c-sidebar u-flex-1 <?php the_field('margin_sidebar') ?>">
+                    <?php dynamic_sidebar($sidebar) ?>
+                </ul>
+            </div>
+
+        <?php else: ?>
+
+            <div class="c-post-content l-container l-container--small">
+                <?php the_content() ?>
+            </div>
+        
+        <?php endif; ?>
         
     <?php endwhile; endif; ?>
 
